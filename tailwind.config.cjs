cc1510fead954ab0cea8c9dd9bcc39b134754f7c
romazherdev <romazherdev@gmail.com>
2022-12-07 22:51:47 +0100

    Change light color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -35,19 +35,6 @@ module.exports = {
       'font-primary': 'var(--font-primary)',
       'font-secondary': 'var(--font-secondary)',
       decorative: 'var(--decorative)',
-      pink: {
-        300: '#d0abde',
-        400: '#d8aff1',
-        700: '#bb52ed',
-      },
-      grey: {
-        400: '#b3b3b3',
-        900: '#3c3945',
-      },
-      blue: '#1772c6',
-      white: '#ebedf3',
-      black: '#140e25',
-      green: '#5fbab0',
     },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
